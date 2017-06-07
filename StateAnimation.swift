535c44b8ed5089366baab6c7b1b274e0484a32b2
ReasonAmu <luongvantiem@gmail.com>
2017-06-07 14:00:01 +0700

    first commit

---
+++ StateAnimation.swift
@@ -0,0 +1,21 @@
+//
+//  StateAnimation.swift
+//  StatesAnimation
+//
+//  Created by ReasonAmu on 6/7/17.
+//  Copyright © 2017 ReasonAmu. All rights reserved.
+//
+
+import UIKit
+
+class StateAnimation: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
